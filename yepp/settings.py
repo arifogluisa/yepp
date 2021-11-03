2e7c919bede4cde4ac58b493ea965980ed3a5ed7
IsaArifoglu <arifogluisa@gmail.com>
2021-11-03 22:58:48 +0400

    besinci commit

--- yepp/settings.py
+++ yepp/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w+7jwg3ukv!^!hpcs*v69x6y^%27f7_iidm4*mb2%4xv%gpcmj'
-
+#SECRET_KEY = 'django-insecure-w+7jwg3ukv!^!hpcs*v69x6y^%27f7_iidm4*mb2%4xv%gpcmj'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
